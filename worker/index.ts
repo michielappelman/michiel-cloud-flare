7462cbdccd14cd90538ee7d345d9f6ec3f21a064
Michiel Appelman <michiel@cloudflare.com>
2022-11-30 22:17:36 +0100

    Seems like a good first release

--- worker/index.ts
+++ worker/index.ts
@@ -1,5 +1,5 @@
 export default {
   async fetch(): Promise<Response> {
-    return fetch("https://michiel-cloud-flare.michielcloud.pages.dev");
+    return fetch("https://michiel-cloud-flare.pages.dev");
   },
 };
